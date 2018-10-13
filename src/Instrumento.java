9b27eb7825fafe02a78a9f2a178d163b7bcefb93
rafa <rjribeiro.inf.ufrgs.br>
2018-10-13 18:58:11 -0300

    correcoes

--- src/Instrumento.java
+++ src/Instrumento.java
@@ -1,4 +1,33 @@
 
-public class Instrumento {
+public abstract class Instrumento {
+	
+	private int BPM = 0;
+	private int volume = 0;
+		
+	
+	abstract void toca_som(String letra);
+	
+	void aumenta_volume(int valor) {
+		this.volume += valor;
+	}
+	
+	void diminiu_volume(int valor) {
+		this.volume -= valor;
+	}
 
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public int getBPM() {
+		return BPM;
+	}
+
+	public void setBPM(int bPM) {
+		BPM = bPM;
+	}
 }
