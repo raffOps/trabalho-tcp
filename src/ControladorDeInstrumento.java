58c9a7d6ce3bb0809dc06a48f46727007ac988c5
rafa <rjribeiro.inf.ufrgs.br>
2018-11-02 19:11:06 -0300

    alteracao de metodos na classe

--- src/ControladorDeInstrumento.java
+++ src/ControladorDeInstrumento.java
@@ -1,42 +1,25 @@
 
 public abstract class ControladorDeInstrumento {
 	
-	private int BPM = 0;
-	private int volume = 0;
-	private int instrumento_id;
+	private int oitava;
 	
 	abstract void toca_som(String letra);
 	
 	
-	void aumenta_volume(int valor) {
-		this.volume += valor;
+
+	public void aumentaOitava() {
+		this.oitava += 1;
 	}
 	
-	void diminiu_volume(int valor) {
-		this.volume -= valor;
+	public void diminuiOitava() {
+		this.oitava -= 1;
 	}
 
-	public int getVolume() {
-		return volume;
+	public int getOitava() {
+		return oitava;
 	}
 
-	public void setVolume(int volume) {
-		this.volume = volume;
-	}
-
-	public int getBPM() {
-		return BPM;
-	}
-
-	public void setBPM(int bPM) {
-		BPM = bPM;
-	}
-
-	public int getInstrumento_id() {
-		return instrumento_id;
-	}
-
-	public void setInstrumento_id(int instrumento_id) {
-		this.instrumento_id = instrumento_id;
+	public void setOitava(int oitava) {
+		this.oitava = oitava;
 	}
 }
