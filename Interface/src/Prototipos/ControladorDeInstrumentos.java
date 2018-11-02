60b169857bbb12adea22b712f8860a48ef3c7fb2
rafa <rjribeiro.inf.ufrgs.br>
2018-11-02 19:43:46 -0300

    exclusao da pasta interface

--- Interface/src/Prototipos/ControladorDeInstrumentos.java
+++
@@ -1,9 +0,0 @@
-package Prototipos;
-public class ControladorDeInstrumentos {
-    
-    public ControladorDeInstrumentos(String texto, int voz, int instrumento, int tempo)
-    {    
-        // Seta instrumentos, troca e faz alterações nos instrumentos.
-    }
-    
-}
--- Interface/src/Prototipos/Volume.java
+++
@@ -1,9 +0,0 @@
-package Prototipos;
-public class Volume {
-    
-    public Volume(int valordoJSlider)
-    {
-        //Controle e ajuste do volume geral da musica.
-    }
-    
-}
