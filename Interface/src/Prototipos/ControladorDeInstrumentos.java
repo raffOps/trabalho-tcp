f3ab4f5c98e7f1e78aa801a61a962810bf27da8b
Gabriel Beretta <44712765+Beretta350@users.noreply.github.com>
2018-11-02 18:22:50 -0300

    Interface do Trabalho

---
+++ Interface/src/Prototipos/ControladorDeInstrumentos.java
@@ -0,0 +1,9 @@
+package Prototipos;
+public class ControladorDeInstrumentos {
+    
+    public ControladorDeInstrumentos(String texto, int voz, int instrumento, int tempo)
+    {    
+        // Seta instrumentos, troca e faz alterações nos instrumentos.
+    }
+    
+}
---
+++ Interface/src/Prototipos/Volume.java
@@ -0,0 +1,9 @@
+package Prototipos;
+public class Volume {
+    
+    public Volume(int valordoJSlider)
+    {
+        //Controle e ajuste do volume geral da musica.
+    }
+    
+}
