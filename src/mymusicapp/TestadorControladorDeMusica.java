3f4866359f7484012fc6de269bc72ae483a6266b
rafa <rjribeiro.inf.ufrgs.br>
2018-11-05 21:30:56 -0200

    classe teste. Relacao entre ControladorDeMusica e Tradutor

--- src/mymusicapp/TestadorControladorDeMusica.java
+++ src/mymusicapp/TestadorControladorDeMusica.java
@@ -7,10 +7,12 @@ public class TestadorControladorDeMusica {
 
 	public static void main(String[] args) {
 
-
+			
 		Tradutor musica = new Tradutor("2ABCD4EF \n2ABCD4EF3ABMCD3EF \n3ABCD3EF");
-		Player player = new Player();
-		Pattern pattern = new Pattern(musica.traduz_musica());
-		player.play(pattern);
+		String musica_traduzida = musica.traduz_musica();
+		System.out.println(musica_traduzida);
+		ControladorDeMusica tocador = new ControladorDeMusica(musica_traduzida);
+		tocador.toca_musica();
+		
 	}
 }
